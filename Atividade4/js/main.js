ec68934332483f524fdbb9489f0748bcf57bf113
Edssaac <edssaac@outlook.com>
2022-03-30 23:22:12 -0300

    implementação de detalhes

--- Atividade4/js/main.js
+++ Atividade4/js/main.js
@@ -1,11 +1,15 @@
-window.onload = () => 
+window.onload = popularPagina;
+
+function popularPagina() 
 {
-
     let ctx = '';
-
 
     for ( let i=0; i< dados.length; i++ )
     {
+        let imagem = dados[i].imagem;
+        if (i<6)
+            imagem = "img/"+imagem;
+
         ctx += 
         `
         <div class="mb-4">
@@ -15,7 +19,7 @@ window.onload = () =>
     
             <div class="row">
                 <figure class="col-xs-12 col-md-6">
-                    <img src="img/${dados[i].imagem}" alt="${dados[i].titulo}" class="img-fluid rounded">
+                    <img src="${imagem}" alt="${dados[i].titulo}" class="img-fluid rounded">
                 </figure>
     
                 <p class="col-xs-12 col-md-6">
@@ -30,3 +34,50 @@ window.onload = () =>
 
     document.querySelector("article").innerHTML = ctx;
 };
+
+
+var form = document.getElementById("form");
+
+var botao = document.getElementById("botao");
+botao.addEventListener("click", botaoFlutuante);
+
+function botaoFlutuante()
+{
+    if ( botao.children[0].className == 'uil uil-plus-circle' )
+    {
+        botao.innerHTML = '<i class="uil uil-minus-circle"></i>';
+
+        form.classList.remove("ocultar");
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+    else
+    {
+        botao.innerHTML = '<i class="uil uil-plus-circle"></i>';
+
+        form.classList.add("ocultar");
+    }
+}
+
+form.addEventListener("submit", function(e) {
+
+    let campo_titulo      = document.getElementById("titulo").value;
+    let campo_descricao   = document.getElementById("descricao").value;
+    let campo_imagem      = document.getElementById("imagem").value;
+    let campo_link        = document.getElementById("link").value;
+
+    // criar objeto:
+    dado = {
+        titulo: campo_titulo,
+        descricao: campo_descricao,
+        imagem: campo_imagem,
+        link: campo_link,
+    }
+
+    dados.push(dado); //adicionar objeto ao array;
+    popularPagina(); //atualizar conteudo;
+
+    form.reset();
+
+    //cancelar envio do formulário:
+    e.preventDefault()
+})
