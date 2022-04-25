b5415bfadd370b54cc589d54ebd532a4da9f8c1a
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2022-04-25 12:37:18 -0300

    Add files via upload

---
+++ Atividade6/js/Media.js
@@ -0,0 +1,21 @@
+const nome = document.getElementById('btnNome');
+const n1 = document.getElementById('btnN1');
+const n2 = document.getElementById('btnN2');
+const n3 = document.getElementById('btnN3');
+
+
+nome.addEventListener('click', () => {
+    
+});
+
+n1.addEventListener('click', () => {
+    
+});
+
+n2.addEventListener('click', () => {
+    
+});
+
+n3.addEventListener('click', () => {
+    
+});
