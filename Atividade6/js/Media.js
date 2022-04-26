e3e0108f3770c52aa4254432c45cc1dff4a5df30
Edssaac <edssaac@outlook.com>
2022-04-26 05:57:59 -0300

    media

--- Atividade6/js/Media.js
+++ Atividade6/js/Media.js
@@ -1,21 +1,39 @@
-const nome = document.getElementById('btnNome');
-const n1 = document.getElementById('btnN1');
-const n2 = document.getElementById('btnN2');
-const n3 = document.getElementById('btnN3');
+const btnNome = document.getElementById('btnNome');
+const btnN1 = document.getElementById('btnN1');
+const btnN2 = document.getElementById('btnN2');
+const btnN3 = document.getElementById('btnN3');
+const btnMedia = document.getElementById('btnMedia');
+
+var nome;
+var n1;
+var n2;
+var n3;
 
 
-nome.addEventListener('click', () => {
-    
+alert("Inserção de Notas e Cálculo de Média");
+
+
+btnNome.addEventListener('click', () => {
+    nome = prompt("Nome:");
+    document.getElementById("nomeAluno").value = nome;
 });
 
-n1.addEventListener('click', () => {
-    
+btnN1.addEventListener('click', () => {
+    n1 = parseFloat(prompt("Nota #1"));
+    document.getElementById("n1").value = n1.toFixed(2);
 });
 
-n2.addEventListener('click', () => {
-    
+btnN2.addEventListener('click', () => {
+    n2 = parseFloat(prompt("Nota #2"));
+    document.getElementById("n2").value = n2.toFixed(2);
 });
 
-n3.addEventListener('click', () => {
-    
+btnN3.addEventListener('click', () => {
+    n3 = parseFloat(prompt("Nota #3"));
+    document.getElementById("n3").value = n3.toFixed(2);
+});
+
+btnMedia.addEventListener('click', () => {
+    let media = ((n1 + n2 + n3) / 3);
+    confirm(`Aluno: ${document.getElementById("nomeAluno").value}\nMédia: ${ (isNaN(media)) ? '' : media.toFixed(2)}`);
 });
---
+++ Atividade6/js/Operacoes.js
@@ -0,0 +1,9 @@
+
+
+// Operacoes.html: 
+// Receba dois números e calcule e mostre:
+// a soma dos dois;
+// a subtração do primeiro pelo segundo;
+// o produto dos dois;
+// a divisão do primeiro pelo segundo;
+// o resto da divisão do primeiro pelo segundo.
