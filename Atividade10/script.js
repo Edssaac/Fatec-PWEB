5f63aad1250618e34956c242f445ad3e6cdb5666
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2022-05-09 12:09:03 -0300

    Add files via upload

--- Atividade10/script.js
+++ Atividade10/script.js
@@ -11,8 +11,19 @@ var aluno3 = {
     nome: 'Edson'
 }
 
+class Aluno {
+
+    constructor(ra, nome) {
+        this.ra = ra;
+        this.nome = nome;
+    }
+}
+
+var aluno4 = new Aluno('Edson', '0030482021020');
+
 console.log('Objeto 1:', aluno1);
 console.log('Objeto 2:', aluno2);
 console.log('Objeto 3:', aluno3);
+console.log('Objeto 4:', aluno4);
 
 alert("Verifique os Objetos no console.");
