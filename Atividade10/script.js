4ce6fff9f04000632762349440b850b00ce695ed
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2022-05-09 11:45:40 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,18 @@
+var aluno1 = new Object();
+aluno1.ra = '0030482021020';
+aluno1.nome = 'Edson';
+
+var aluno2 = {};
+aluno2.ra = '0030482021020';
+aluno2.nome = 'Edson';
+
+var aluno3 = {
+    ra: '0030482021020',
+    nome: 'Edson'
+}
+
+console.log('Objeto 1:', aluno1);
+console.log('Objeto 2:', aluno2);
+console.log('Objeto 3:', aluno3);
+
+alert("Verifique os Objetos no console.");
---
+++ Atividade9/script.js
@@ -0,0 +1,35 @@
+const btnMaior = document.getElementById("btnMaior");
+const btnOrdenar = document.getElementById("btnOrdenar");
+const n1 = document.getElementById("n1");
+const n2 = document.getElementById("n2");
+const n3 = document.getElementById("n3");
+
+btnMaior.addEventListener('click', () => {
+    alert("Maior Número: " + retornaMaior(n1.value, n2.value, n3.value));
+});
+
+btnOrdenar.addEventListener('click', () => {
+    ordenaNumeros(n1.value, n2.value, n3.value);
+});
+
+// Função responsável por ordenar os números em ordem crescente:
+function ordenaNumeros(a, b, c) {
+
+    let ordenados = [a, b, c];
+
+    ordenados.sort((x, y) => {
+        return (x - y);
+    });
+
+    n1.value = ordenados[0];
+    n2.value = ordenados[1];
+    n3.value = ordenados[2];
+
+    return ordenados;
+}
+
+// Função responsável por retornar o maior número:
+function retornaMaior(a, b, c) {
+
+    return ordenaNumeros(a, b, c)[2];
+}
