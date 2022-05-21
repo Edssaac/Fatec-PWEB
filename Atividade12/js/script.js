6886fcb190e126453a6166297f2982697e2ec813
Edssaac <edssaac@outlook.com>
2022-05-21 17:16:56 -0300

    .

---
+++ Atividade12/js/script.js
@@ -0,0 +1,17 @@
+const imagem = document.getElementById("imagem");
+
+onload = () => {
+    imagem.src = "img/janelafechada.png";
+}
+
+imagem.addEventListener("mouseenter", () => {
+    imagem.src = "img/janelaaberta.png";
+})
+
+imagem.addEventListener("mouseleave", () => {
+    imagem.src = "img/janelafechada.png";
+})
+
+imagem.addEventListener("click", () => {
+    imagem.src = "img/janelaquebra.png";
+})
