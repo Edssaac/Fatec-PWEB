c7ede92c7d04725fa0454cb88e01361e32c19f71
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2022-06-01 11:09:32 -0300

    Add files via upload

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -16,3 +16,25 @@ function scrollTop() {
 }
 
 window.addEventListener('scroll', scrollTop);
+
+
+linksCertificado = document.getElementsByClassName("certificado");
+certificado = document.getElementById("certificado");
+
+
+Array.from(linksCertificado).forEach(link => {
+    link.addEventListener("click", () => {
+        certificado.src = certificados[link.id];
+    });
+});
+
+
+certificados = {
+    0: 'assets/img/cursos/introducao_php.png',
+    1: 'assets/img/cursos/poo_php.png',
+    2: 'assets/img/cursos/avancado_php.png',
+    3: 'assets/img/cursos/git.png',
+    4: 'assets/img/cursos/scrum.png',
+    5: 'assets/img/cursos/figma.png',
+    6: 'assets/img/cursos/bootstrap.png'
+}
