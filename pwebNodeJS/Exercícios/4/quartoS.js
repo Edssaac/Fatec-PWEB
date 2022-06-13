00227793af2dde817df0199dd5b6b64be5bf9976
Edssaac <edssaac@outlook.com>
2022-06-13 14:33:52 -0300

    ajustes pastas

--- pwebNodeJS/Exercícios/4/quartoS.js
+++
@@ -1,8 +0,0 @@
-// impotando o módulo fs:
-const fs = require('fs');
-
-// lendo o arquivo "file.txt":
-const data = fs.readFileSync('file.txt');
-
-// execução bloqueada até o termino da leitura:
-console.log(data.toString());
